c3c824b6cea461a4c42eed1bb8120da413094bcb
Valdemar Pereira <valdemar.pereira@gmail.com>
2017-05-10 23:09:06 +0100

    add utest

---
+++ src/test/java/com/valdemar/retropoints/endpoint/RetropointControllerTest.java
@@ -0,0 +1,60 @@
+package com.valdemar.retropoints.endpoint;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RetropointControllerTest {
+//https://github.com/spring-guides/gs-accessing-mongodb-data-rest/blob/master/complete/src/test/java/hello/ApplicationTests.java
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testPing() throws Exception {
+        this.mockMvc
+                .perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("I'm Alive"));
+    }
+
+    @Test
+    public void shouldCreateRetropoint() throws Exception {
+
+
+    }
+
+
+    /*
+     @Test
+    public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
+
+        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, World!"));
+    }
+
+    @Test
+    public void paramGreetingShouldReturnTailoredMessage() throws Exception {
+
+        this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
+    }
+     */
+
+}
